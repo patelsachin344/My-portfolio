f769431dcd4e572222c172d1ad50d02c9467538d
patelsachin344 <sachinpatel71344@gmail.com>
2023-02-02 01:49:09 +0530

    some change

--- src/components/ExternalComponents/demoProjectArray.jsx
+++ src/components/ExternalComponents/demoProjectArray.jsx
@@ -33,4 +33,34 @@ export const data = [
     about:
       "Milaap is an online crowdfunding platform that enables anyone across India to raise funds for healthcare, education, sports, disaster relief and other personal causes.",
   },
+  {
+    id: 1,
+    image: img1,
+    title: "NordSrome",
+    github: "https://github.com/patelsachin344/Nordstrome-clone",
+    demo: "https://clone-nordstrom.netlify.app/",
+    teckStacks: "React | Redux | Chakra Ui | Third-Party Libraries",
+    about:
+      "This is an E-Commerce website which speciaizes in selling clothes and styling products. The website provides 800 brands and 33,000 products of different types of clothes, watches, sunglasses, shoes etc.",
+  },
+  {
+    id: 2,
+    image: img2,
+    title: "Nykaa",
+    github: "https://github.com/patelsachin344/crispy-naykaa",
+    demo: "https://clone-nykaa.netlify.app/",
+    teckStacks: "HTML | CSS | JavaScript",
+    about:
+      "Nykaa is an Indian e-commerce company, headquartered in Mumbai. It sells beauty, wellness and fashion products across websites, mobile apps and 100+ offline stores.",
+  },
+  {
+    id: 3,
+    image: img3,
+    title: "Millap",
+    github: "https://github.com/patelsachin344/Milaap_clone",
+    demo: "https://milaapclone.netlify.app/index.html",
+    teckStacks: "HTML | CSS | JavaScript",
+    about:
+      "Milaap is an online crowdfunding platform that enables anyone across India to raise funds for healthcare, education, sports, disaster relief and other personal causes.",
+  },
 ];
